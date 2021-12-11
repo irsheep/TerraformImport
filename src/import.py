5c60d832077ab446af6f737c97f9ff93c57d2f5c
Luis Tavares <github@sheeponline.net>
2021-12-11 22:34:58 +0000

    Added ImportFromTfState to import data from terraform.tfstate file to the '.tf' with the resources

--- src/import.py
+++ src/import.py
@@ -5,9 +5,15 @@ JSON_DATA_FILE = "data/provider_data.json"
 # Output script for 'terraform import'
 TERRAFORM_IMPORT_SCRIPT = "data/import.sh"
 # Skeleton .tf file for the imported resources
-TERRAFORM_OUTPUT_FILE = "data/import.tf"
-# Terraform provider resource stype
-TF_RESOURCE_TYPE = "azurerm_backup_protected_file_share"
+TERRAFORM_SKELETON_TF_FILE = "data/skeleton.tf"
+# Final terraform file with full imported resources
+TERRAROFM_RESOURCE_TF_FILE="data/resources.tf"
+# Terraform state file, this can be the latest copy
+TERRAFORM_STATE_FILE="data/terraform.tfstate"
+# JSON file with attrubites per resource to import to TERRAROFM_RESOURCE_TF_FILE
+TERRAFORM_MAPPING_FILE="data/mapping.json"
+# Terraform provider resource type
+TERRAFORM_RESOURCE_TYPE = "azurerm_backup_protected_file_share"
 # Provider resource filter
 PROVIDER_RESOURCE_TYPE = "AzureFileShare"
 # Dot path to the resource name
@@ -19,13 +25,22 @@ def LoadJsonFile(fileName):
   data = fileHandle.read()
   return json.loads(data)
 
+def LoadTextFile(fileName):
+  fileHandle = open(fileName, "r")
+  data = fileHandle.read()
+  return data
+
 # Writes an array to a text file
-def WriteDataToFile(filename, data):
+def WriteArrayToFile(filename, data):
   with open(filename, "w") as fileHandle:
     for element in data:
       fileHandle.write('%s\n' % element)
 
-# Get the value from a Dict a key in a dot path "a.b.c" 
+def WriteTextToFile(filename, data):
+  with open(filename, "w") as fileHandle:
+    fileHandle.write(data)
+
+# Get the value from a Dict a key in a dot path "a.b.c"
 def GetDictValueFromDotKeyPath(object, dotKeyPath):
   if isinstance(object, str): return object
   if isinstance(dotKeyPath, str): dotKeyPath = dotKeyPath.split(".")
@@ -42,8 +57,7 @@ def GetDictValueFromDotKeyPath(object, dotKeyPath):
   else:
     return 3
 
-# Entrypoint
-def main():
+def CreateImportFiles():
   data = LoadJsonFile(JSON_DATA_FILE)
   importData=[]
   terraformData=[]
@@ -51,10 +65,45 @@ def main():
     if PROVIDER_RESOURCE_TYPE in resource['name']:
       TF_RESOURCE_NAME=GetDictValueFromDotKeyPath(resource, PROVIDER_RESOURCE_NAME_KEY)
       PROVIDER_RESOURCE_ID=resource['id']
-      importData.append(f"terraform import {TF_RESOURCE_TYPE}.{TF_RESOURCE_NAME} {PROVIDER_RESOURCE_ID}")
-      terraformData.append(f"resource \"{TF_RESOURCE_TYPE}\" \"{TF_RESOURCE_NAME}\" {{}}\n")
-  WriteDataToFile(TERRAFORM_IMPORT_SCRIPT, importData)
-  WriteDataToFile(TERRAFORM_OUTPUT_FILE, terraformData)
+      importData.append(f"terraform import {TERRAFORM_RESOURCE_TYPE}.{TF_RESOURCE_NAME} {PROVIDER_RESOURCE_ID}")
+      terraformData.append(f"resource \"{TERRAFORM_RESOURCE_TYPE}\" \"{TF_RESOURCE_NAME}\" {{}}\n")
+  WriteArrayToFile(TERRAFORM_IMPORT_SCRIPT, importData)
+  WriteArrayToFile(TERRAFORM_SKELETON_TF_FILE, terraformData)
+
+def FindResourceInstancesFromTfstate(tfState, type, name):
+  for resource in tfState['resources']:
+    if resource['type'] == type and resource['name'] == name:
+      # instances[0] is a terrible assumption
+      return resource['instances'][0]['attributes']
+  return False
+
+def CastValueToTerraformType(value):
+  if value == None: return "null"
+  if isinstance(value, bool): return f"{str(value).lower()}"
+  if isinstance(value, int): return int(value)
+  return f'"{value}"'
+
+def ImportFromTfState():
+  terraformStateData = LoadJsonFile(TERRAFORM_STATE_FILE)
+  importData = LoadTextFile(TERRAFORM_SKELETON_TF_FILE)
+  mappings = LoadJsonFile(TERRAFORM_MAPPING_FILE)
+  terraformResourceData = ""
+  for line in importData.split("\n"):
+    if line != "":
+      [ a, resourceType, resourceName, d] = line.replace('"',"").split()
+      tfstateInstanceAttributes = FindResourceInstancesFromTfstate(terraformStateData, resourceType, resourceName)
+      terraformResourceData = f'{terraformResourceData}resource "{resourceType}" "{resourceName}" {{'
+      if tfstateInstanceAttributes:
+        for key in mappings[resourceType]:
+          terraformResourceData = f'{terraformResourceData}\n  {key} = {CastValueToTerraformType(tfstateInstanceAttributes[key])}'
+        terraformResourceData = f'{terraformResourceData}\n'
+      terraformResourceData = f'{terraformResourceData}}}\n\n'
+  WriteTextToFile(TERRAROFM_RESOURCE_TF_FILE, terraformResourceData)
+
+# Entrypoint
+def main():
+  CreateImportFiles()
+  ImportFromTfState()
 
 if __name__ == "__main__":
   main()
