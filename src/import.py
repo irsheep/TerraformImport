fe4817023970809a4a1db67c860399a6c2add3ef
Luis Tavares <github@sheeponline.net>
2021-12-12 11:00:00 +0000

    Changes to script and Dockerfile
    Global variables are now defined in settings.cfg
    Implemented command line arguments
    Python requirements are installed when image is built

--- src/import.py
+++ src/import.py
@@ -1,23 +1,25 @@
 import json
+import argparse
+import configparser
 
 # JSON data exported from the provider, for the resources to import
-JSON_DATA_FILE = "data/provider_data.json"
+JSON_DATA_FILE = ""
 # Output script for 'terraform import'
-TERRAFORM_IMPORT_SCRIPT = "data/import.sh"
+TERRAFORM_IMPORT_SCRIPT = ""
 # Skeleton .tf file for the imported resources
-TERRAFORM_SKELETON_TF_FILE = "data/skeleton.tf"
+TERRAFORM_SKELETON_TF_FILE = ""
 # Final terraform file with full imported resources
-TERRAROFM_RESOURCE_TF_FILE="data/resources.tf"
+TERRAROFM_RESOURCE_TF_FILE = ""
 # Terraform state file, this can be the latest copy
-TERRAFORM_STATE_FILE="data/terraform.tfstate"
+TERRAFORM_STATE_FILE = ""
 # JSON file with attrubites per resource to import to TERRAROFM_RESOURCE_TF_FILE
-TERRAFORM_MAPPING_FILE="data/mapping.json"
+TERRAFORM_MAPPING_FILE = ""
 # Terraform provider resource type
-TERRAFORM_RESOURCE_TYPE = "azurerm_backup_protected_file_share"
+TERRAFORM_RESOURCE_TYPE = ""
 # Provider resource filter
-PROVIDER_RESOURCE_TYPE = "AzureFileShare"
+PROVIDER_RESOURCE_TYPE = ""
 # Dot path to the resource name
-PROVIDER_RESOURCE_NAME_KEY = "properties.friendlyName"
+PROVIDER_RESOURCE_NAME_KEY = ""
 
 # Loads json data from a file
 def LoadJsonFile(fileName):
@@ -25,6 +27,7 @@ def LoadJsonFile(fileName):
   data = fileHandle.read()
   return json.loads(data)
 
+# Loads the contents of a file to a string
 def LoadTextFile(fileName):
   fileHandle = open(fileName, "r")
   data = fileHandle.read()
@@ -36,6 +39,7 @@ def WriteArrayToFile(filename, data):
     for element in data:
       fileHandle.write('%s\n' % element)
 
+# Writes a text string to a file, overwiting its contents
 def WriteTextToFile(filename, data):
   with open(filename, "w") as fileHandle:
     fileHandle.write(data)
@@ -100,10 +104,41 @@ def ImportFromTfState():
       terraformResourceData = f'{terraformResourceData}}}\n\n'
   WriteTextToFile(TERRAROFM_RESOURCE_TF_FILE, terraformResourceData)
 
+def LoadConfig():
+  global JSON_DATA_FILE
+  global TERRAFORM_IMPORT_SCRIPT
+  global TERRAFORM_SKELETON_TF_FILE
+  global TERRAFORM_RESOURCE_TYPE
+  global TERRAFORM_STATE_FILE
+  global TERRAFORM_MAPPING_FILE
+  global PROVIDER_RESOURCE_TYPE
+  global PROVIDER_RESOURCE_NAME_KEY
+
+  cfg = configparser.RawConfigParser()
+  cfg.optionxform = lambda option: option # Prevent config parser from changing the CASE of variable names
+  cfg.read('settings.cfg')
+  settings=dict(cfg.items("Settings"))
+  for setting in settings:
+    settings[setting]=settings[setting].split("#",1)[0].strip() # To get rid of inline comments
+  globals().update(settings)  # Make them availible globally
+
 # Entrypoint
 def main():
-  CreateImportFiles()
-  ImportFromTfState()
+  LoadConfig()
+
+  parser = argparse.ArgumentParser(description='Import existing provider resources to Terraform')
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument("-c", "--create", action="store_true", help="Create 'terraform import' script and file for resources")
+  group.add_argument("-m", "--merge", action="store_true", help=f"Merge properties from terraform state file to '{TERRAROFM_RESOURCE_TF_FILE}'")
+
+  args = parser.parse_args()
+
+  if args.create:
+    CreateImportFiles()
+  elif args.merge:
+    ImportFromTfState()
+  else:
+    args = parser.parse_args(['-h'])
 
 if __name__ == "__main__":
   main()
